671354bec592d2145f5a9a7e9972962aa9b551ba
Mei <52403980+alice890308@users.noreply.github.com>
2021-03-20 01:21:38 +0800

    Update and rename median-300. Longest Increasing Subsequence(O(N^2)).cpp to median-300. Longest Increasing Subsequence.cpp

--- binary-search/median-300. Longest Increasing Subsequence.cpp
+++ binary-search/median-300. Longest Increasing Subsequence.cpp
@@ -33,3 +33,30 @@ public:
         return cur_longest;
     }
 };
+
+/*
+O(NlgN) solution
+1. 陣列arr中儲存目前被挑選到的數字。
+2. lower_bound會用二分搜找到大於等於指定數值的第一個數字。
+3. 遍歷nums，搜尋arr中是否有比當前數字更大的數
+    3-1. 若無，則直接把當前數字加到arr中
+    3-2. 若有，則把該數字的位置用當前數字取代(為了不破壞increasing subsequence在nums中的順序)
+*/
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> arr;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            if (arr.empty() || nums[i] > arr.back()) {
+                arr.push_back(nums[i]);
+            }
+            else {
+                *lower_bound(arr.begin(), arr.end(), nums[i]) = nums[i];
+            }
+        }
+        
+        return arr.size();
+    }
+};
