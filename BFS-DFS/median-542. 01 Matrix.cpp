47181694899bee544bd009e9d8fee94107bf1feb
Mei <52403980+alice890308@users.noreply.github.com>
2021-03-27 15:19:24 +0800

    Update median-542. 01 Matrix.cpp

--- BFS-DFS/median-542. 01 Matrix.cpp
+++ BFS-DFS/median-542. 01 Matrix.cpp
@@ -1,10 +1,9 @@
-/* IDDFS version */
+/* IDDFS */
 
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
         vector<vector<int> > updated(matrix.size(), vector<int> (matrix[0].size()));
-        vector<vector<int> > tried(matrix.size(), vector<int> (matrix[0].size())); // 紀錄每個位置已經嘗試過走幾步了
         vector<vector<bool> > visited(matrix.size(), vector<bool> (matrix[0].size()));
         int steps;
         bool temp;
@@ -16,7 +15,6 @@ public:
                     updated[i][j] = 0;
                 else
                     updated[i][j] = -1;
-                tried[i][j] = 0;
                 visited[i][j] = false;
             }
         }
@@ -24,10 +22,9 @@ public:
             for(int j = 0; j < matrix[i].size(); j++) {
                 if (updated[i][j] != -1)
                     continue;
-                steps = tried[i][j];
+                steps = 0;
                 while(++steps) {
-                    //cout << " i = " << i << " , j = " << j << '\n';
-                    temp = IDDFS(matrix, tried, visited, updated, i, j, steps);
+                    temp = IDDFS(matrix, visited, updated, i, j, steps);
                     if (temp)
                         break;
                 }
@@ -37,9 +34,11 @@ public:
     }
 
 private:
-    bool IDDFS(vector<vector<int>>& matrix, vector<vector<int>>& tried, vector<vector<bool>>& visited, vector<vector<int>>& updated, int row, int col, int limit_steps) {
+    bool IDDFS(vector<vector<int>>& matrix, vector<vector<bool>>& visited, vector<vector<int>>& updated, int row, int col, int limit_steps) {
         bool temp;
         
+        if (row < 0 || row >= matrix.size()) return false;
+        if (col < 0 || col >= matrix[row].size()) return false;
         if (limit_steps == updated[row][col]) {
             return true;
         }
@@ -51,44 +50,16 @@ private:
                 return true;
             return false;
         }
-        
-        if (limit_steps > tried[row][col])
-            tried[row][col] = limit_steps;
-        
+
         visited[row][col] = true;
-        if (row+1 < matrix.size() && visited[row+1][col] == false) {
-            temp = IDDFS(matrix, tried, visited, updated, row+1, col, limit_steps-1);
-            if (temp == true) {
-                updated[row][col] = limit_steps;
-                visited[row][col] = false;
-                return true;
-            }
-        }
-        if (row-1 >= 0 && visited[row-1][col] == false) {
-            //cout << "row-1 = " << row-1 << " , col = " << col << '\n';
-            temp = IDDFS(matrix, tried, visited, updated, row-1, col, limit_steps-1);
-            //cout << temp << '\n';
-            if (temp == true) {
-                updated[row][col] = limit_steps;
-                visited[row][col] = false;
-                return true;
-            }
-        }
-        if (col-1 >= 0 && visited[row][col-1] == false) {
-            temp = IDDFS(matrix, tried, visited, updated, row, col-1, limit_steps-1);
-            if (temp == true) {
-                updated[row][col] = limit_steps;
-                visited[row][col] = false;
-                return true;
-            }
-        }
-        if (col+1 < matrix[row].size() && visited[row][col+1] == false) {
-            temp = IDDFS(matrix, tried, visited, updated, row, col+1, limit_steps-1);
-            if (temp == true) {
-                updated[row][col] = limit_steps;
-                visited[row][col] = false;
-                return true;
-            }
+        temp = IDDFS(matrix, visited, updated, row+1, col, limit_steps-1);
+        if (!temp) temp = IDDFS(matrix, visited, updated, row-1, col, limit_steps-1);
+        if (!temp) temp = IDDFS(matrix, visited, updated, row, col-1, limit_steps-1);
+        if (!temp) temp = IDDFS(matrix, visited, updated, row, col+1, limit_steps-1);
+        if (temp) {
+            updated[row][col] = limit_steps;
+            visited[row][col] = false;
+            return true;
         }
         visited[row][col] = false;
         return false;
